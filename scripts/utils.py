e58adb8fdcb252db6f1a826d37929dbec7260ca5
jmaiolini <jacopo.maiolini@gmail.com>
2021-03-18 19:46:01 +0100

    reproduced waypoints

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,6 +4,35 @@ import json
 import sys
 import os.path
 import rospkg
+import cv2
+
+#PROBLEM: we have mismatching coordinates as follows
+# reasoning to make it generic to images and stores.
+
+#   _____________________________
+#   |  ________________________  |
+#   | |                        | |
+#   | |                        | |
+#   | |                        | |
+#   | |________________________| |
+#   |____________________________|
+
+#we have that:
+# the outer is given by image pixels
+# the inner is given by the store map
+
+
+#TODO modify them based on the store (for now only store B available)
+# px_m_ratio_x = 0.026529338 #m/px WRONG
+# px_m_ratio_y = 0.02677686 #m/px WRONG
+
+
+store_px_w = 1602 #store pixels width 
+store_px_h = 1210 #store pixels height
+store_m_w = 42.5 #store meter width
+store_m_h = 32.4 #store meter height
+
+
 
 def print_usage(exit_code = 0):
     print '''Usage: rosrun store_gfk wpoints_generator.py <filename>
@@ -17,7 +46,7 @@ def load_trajectory(filename):
     rospack = rospkg.RosPack()
     filepath = rospack.get_path('store_gfk') + '/trajectories/edeka/' + filename
 
-    
+ 
     if os.path.exists(filepath):
         with open(filepath) as f:
             data = json.load(f)
@@ -29,32 +58,36 @@ def load_trajectory(filename):
 
 def transform_waypoints(store_waypoints):
     robot_waypoints = list()
+    
+    
+    planimetry_width = 42.5
+    planimetry_height = 32.4
+    img_width = 1602
+    img_height = 1210
 
     #TODO need also pixels and map meter sizes to calculate ratio. For now hardcoded
-    x_off_px = 22
-    y_off_px = 24
-    store_width_px = 1602 
-    store_height_px = 1210 
-    store_width_m = 42.5 
-    store_height_m = 32.4 
-    
-    [x_off, y_off] = traj_meter_shift(x_off_px, y_off_px, store_width_px, store_height_px,store_width_m, store_height_m)
 
+    px_m_ratio_x = planimetry_width / (img_width-22-30)
+    px_m_ratio_y = planimetry_height / (img_height-24-26)
+ 
+    store_width_m = img_width * px_m_ratio_x #42.5 
+    store_height_m = img_height * px_m_ratio_y #32.4 
+    
+    
     for coord in store_waypoints: #dict type
-        new_coord = tranform_position( coord['x'], coord['y'], x_off, y_off, store_width_m, store_height_m )
+        new_coord = tranform_position( coord['x'], coord['y'], store_width_m, store_height_m )
         robot_waypoints.append( new_coord )
 
     print(robot_waypoints)
-    return robot_waypoints
+    return robot_waypoints, px_m_ratio_x, px_m_ratio_y
     
-def tranform_position(x_traj, y_traj, x_off, y_off, store_width, store_height): 
-    
-    x_traj += x_off
-    y_traj += y_off
-    
-    x_map = x_traj
+def tranform_position(x_traj, y_traj, store_width, store_height): 
+    # x_traj += x_off
+    # y_traj += y_off
+
+    x_map = x_traj 
     y_map = store_height - y_traj
-    
+        
     if x_map >= store_width or y_map >= store_height:
         print('Trajectory out of store map. Aborting')
         sys.exit(1)
@@ -63,14 +96,57 @@ def tranform_position(x_traj, y_traj, x_off, y_off, store_width, store_height):
         print('Negative position. Aborting')
         sys.exit(1)
 
-
     return ( x_map, y_map )
 
-def traj_meter_shift(x,y,w_px,h_px,w_m,h_m): #calculates shift between trajectory and map origins
-    
-    x_off = x*w_m/w_px
-    y_off = y*h_m/h_px
 
-    return round(x_off,3),round(y_off,3)
+def pixels2meters(x_px,y_px, x_ratio, y_ratio):
+    x_m = x_px * x_ratio
+    y_m = y_px * y_ratio
+
+    return x_m,y_m
+
+def meters2pixels(x_m,y_m, x_ratio, y_ratio):
+    x_px = x_m / x_ratio
+    y_px = y_m / y_ratio
+
+    return x_px,y_px
+
+#changes coordinates of a point in meter from image to map coordinates
+def image2map(p_x,p_y, x_ratio, y_ratio):
+    x_map = p_x * x_ratio
+    y_map = store_m_h - p_y * y_ratio
+
+    return x_m,y_m
+#changes coordinates of a point in pixels from map to image coordinates
+def map2image(p_x,p_y, x_ratio, y_ratio):
+    x_img = int(p_x / x_ratio)
+    y_img = store_px_h - int(p_y / y_ratio)
+
+    return x_img,y_img
+
+
+######TESTS (trajectories diferrence must be around (0.6, 0.65) meter )
+def check_traj_correspondences( robot_trajectory, filename, x_ratio, y_ratio):
+
+    rospack = rospkg.RosPack()
+    filepath = rospack.get_path('store_gfk') + '/trajectories/edeka/' + filename
+    filepath = os.path.splitext(filepath)[0]+'.jpg'
+    
+    robot_traj_img = cv2.imread(filepath,1)
+
+    for pose in robot_trajectory:
+        x,y = map2image(pose[0],pose[1], x_ratio, y_ratio)
+        robot_traj_img = cv2.circle(robot_traj_img, (x,y), radius=5, color=(0, 0, 255), thickness=-1)
+
+    robot_traj_img = cv2.circle(robot_traj_img, (22,24), radius=1, color=(0, 0, 255), thickness=-1)
+    robot_traj_img = cv2.circle(robot_traj_img, (1602-30,1210-26), radius=1, color=(0, 0, 255), thickness=-1)
+    cv2.namedWindow("Trajectories",cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Trajectories", int(1602/2), int(1210/2))
+    cv2.imshow("Trajectories", robot_traj_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+    cv2.imwrite("/home/majo/catkin_ws/src/store_gfk/prova.jpg",robot_traj_img)
+    
 
 
--- scripts/wpoints_generator.py
+++ scripts/wpoints_generator.py
@@ -4,56 +4,99 @@ import rospy
 import sys
 import argparse
 import actionlib
-from std_msgs.msg import String
-from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionFeedback
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import OccupancyGrid
+from sensor_msgs.msg import LaserScan
 import utils
 
-class generator:
+class trajectoryGenerator:
 
-    def __init__(self, filename):
-        self.sub = rospy.Subscriber("chatter",String,self.callback)
+    def __init__(self, filename, debug_mode=False):
 
         #transform and load current trajectory
-        self.full_trajectory = utils.load_trajectory(filename)
+        self.full_trajectory, x_ratio, y_ratio = utils.load_trajectory(filename)
+
+        if debug_mode:
+            utils.check_traj_correspondences(self.full_trajectory, filename, x_ratio, y_ratio) #find better way to pass ratios
+        
+        sys.exit(0)
+        #with this parameter we select how far the robot can stop if the goal is inside a wall
+        self.goal_tollerance = 1 #radious in meters
+
+        #ROS interface
         #action for move_base
         self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
         self.client.wait_for_server()
-
         self.goal = MoveBaseGoal()
+        
+        #subscribers
+        self.occupancy_grid_sub = rospy.Subscriber("/map",OccupancyGrid, self.map_cb)
+        self.laer_scan_sub = rospy.Subscriber("/scan",LaserScan, self.laser_cb)
+        self.laser_msg = LaserScan()
+        
+        
 
-        for pose in full_trajectory:
-            self.goal.target_pose.header.frame_id = "map"
-            self.goal.target_pose.header.stamp = rospy.Time.now()
-            self.goal.target_pose.pose.position.x = pose[0]
-            self.goal.target_pose.pose.position.y = pose[1]
-            self.goal.target_pose.pose.position.z = 0.0
-            self.goal.target_pose.pose.orientation.w = 1.0 #not sending orientation, we will rotate to align at the end 
 
-        self.client.send_goal(goal)
+    def start(self):
+        
+        self.goal.target_pose.header.frame_id = "map"
+        self.goal.target_pose.pose.position.z = 0.0
+        self.goal.target_pose.pose.orientation.x = 0.0
+        self.goal.target_pose.pose.orientation.y = 0.0
+        self.goal.target_pose.pose.orientation.z = 0.0
+        self.goal.target_pose.pose.orientation.w = 1.0 #not sending orientation, we will rotate to align at the end
+
+        # for waypoint in self.full_trajectory:
+            # send_waypoint(self.goal, waypoint[0], waypoint[1])
+        self.goal.target_pose.header.stamp = rospy.Time.now()
+        self.goal.target_pose.pose.position.x = self.full_trajectory[0][0]
+        self.goal.target_pose.pose.position.y = self.full_trajectory[0][1]
+
+
+        print 'Current Goal: ( ', self.full_trajectory[0][0] , ',' , self.full_trajectory[0][1], ' )'
+        
+        #send the current goal
+        self.client.send_goal(self.goal,feedback_cb=self.feedback_cb) #it is possible to add the others callbacks directly from here
+        
         wait = self.client.wait_for_result()
-
+        
         if not wait:
             rospy.logerr("Action server not available!")
             rospy.signal_shutdown("Action server not available!")
         else:
             print(self.client.get_result())
 
+        # align_robot()
 
+    # def send_waypoint(self,goal, x_waypoint, y_waypoint):
+    # ...
+    
+    # def align_robot(self):
+    # ...
 
-    def callback(self, data):
-        print(data.data)
+    def feedback_cb(self,feedback):
+        # rospy.loginfo("Feedback:%s" % str(feedback))
+        print("\n")
+        
+    def map_cb(self,data):
+        print("Getting map data.\n")
 
+    def laser_cb(self,data):
+        self.laser_msg = data.data
 
 
 def main():
     
     args = rospy.myargv()
-    print(args[1])
+    
     if len(args) != 2:
         utils.print_usage()
     rospy.init_node('wpoints_generator', anonymous=True)
 
-    obc = generator(args[1])
+    robot_navigation = trajectoryGenerator(args[1], True)
+    robot_navigation.start()
+
     
     try:
         rospy.spin()
